b6804d1fe7e0d182a77e865cffefe029636845fb
LORDyyyyy <mlord6271@gmail.com>
2023-04-23 19:51:32 +0000

    Task #0

--- print_them.c
+++ print_them.c
@@ -4,42 +4,42 @@
  * print_them - takes the format from _printf and control the %
  * @format: the full string from printf
  * @ap: all argumentents in _printf
- * @goto_func: holds specifiers and functions' pointers
+ * @go_to_function: holds specifiers and functions' pointers
  *
  * Return: the length
  */
-int print_them(const char *format, va_list ap, formats_t goto_func[])
+int print_them(const char *format, va_list ap, formats_t go_to_function[])
 {
-	int i, j, printed_count = 0, len_function, flag;
+	int i, j, printed_count = 0, len_function;
 
 	for (i = 0; format[i]; i++)
 	{
-		flag = 0;
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
 				return (-1);
-			for (j = 0; goto_func[j].scp; j++)
+			for (j = 0; go_to_function[j].scp; j++)
 			{
-				if (format[i + 1] == goto_func[j].scp[0])
+				if (format[i + 1] == go_to_function[j].scp[0])
 				{
-					len_function = goto_func[j].f(ap);
-					if (len_function == -1)
-						return (-1);
-					flag = 1;
+					len_function = go_to_function[j].f(ap);
 					printed_count += len_function;
 					break;
 				}
 			}
-			if (flag == 0 && format[i + 1])
+			if (!go_to_function[j].scp)
 			{
-				_writechar(format[i]);
-				_writechar(format[i + 1]);
-				printed_count += 2;
-			}
-			else
-			{
-				return (-1);
+				if (format[i + 1])
+				{
+					_writechar(format[i]);
+					_writechar(format[i + 1]);
+					printed_count += 2;
+				}
+				else
+				{
+					_writechar(format[i]);
+					printed_count++;
+				}
 			}
 			i++;
 		}
