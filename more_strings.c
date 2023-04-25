97150ec6d43b2811eb478ace38d7becb7a6d7b88
mostafa <mostafa51moktar@gmail.com>
2023-04-25 05:02:24 -0700

    task #13 #14 ,M

--- more_strings.c
+++ more_strings.c
@@ -25,3 +25,37 @@ int print_rev(va_list ap, params_t *params)
 	}
 	return (printed);
 }
+
+/**
+ * print_rot13 - prints a string in rot13 rotation
+ * @ap: passed argument
+ * @params: parameters
+ *
+ * Return: string length
+ */
+
+int print_rot13(va_list ap, params_t *params)
+{
+	int i, j, len, flag = 1;
+	char ORIGINAL[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char ROTATED[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+	char *s = va_arg(ap, char *);
+	(void)params;
+
+	len = _strlen(s);
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; j < 52; j++)
+			if (s[i] == ORIGINAL[j])
+		{
+				putchar(ROTATED[j]);
+				flag = 0;
+				break;
+		}
+		if (flag)
+			putchar(s[i]);
+		flag = 1;
+	}
+	return (len);
+}
